23d83527b8b57e179663c934ff4131d727b2d243
Ernest Buffington <ernest.buffington@gmail.com>
2022-11-04 15:31:13 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,19 @@
 <?php
-$dmbs = 'mysql';
-$table_prefix = 'phpbb';
-$dbhost = 'localhost';
-$dbname = 'phpbbfm_db';
-$dbuname = 'phpbbfm_user';
-$dbpasswd = 'xwdNPADv86fm';
+
+//
+// phpBB 2.x auto-generated config file
+// Do not change anything in this file!
+//
+
+$dbms = 'mysql';
+
+$dbhost = 'localhost'; // Enter your database host, 'localhost' in most cases
+$dbname = 'phpbbfm_db'; // Enter your database name
+$dbuname = 'phpbbfm_user'; // Enter your database username
+$dbpasswd = ''; // Enter your database password
+
+$table_prefix = 'phpbb_'; // Enter your database table prefix
 
 define('PHPBB_INSTALLED', true);
 
+?>
